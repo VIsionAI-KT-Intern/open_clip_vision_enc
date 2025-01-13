75eec10a0b9c9132b6d0c2b3a2ad2b70a2b24671
Chaewon829 <lcwon9@gmail.com>
2025-01-13 02:40:23 +0000

    add 3k data sh file

---
+++ scripts/vit_b_32_3k.sh
@@ -0,0 +1,21 @@
+python3 /home/work/VisionAI_Intern_Project/open_clip_vision_enc/src/open_clip_train/main.py \
+    --model ViT-B-32 \
+    --name vit-B-32_3k \
+    --image-mean 0.556 0.531 0.504 \
+    --image-std 0.326 0.321 0.334 \
+    --seed 0 \
+    --save-frequency 1 \
+    --dataset-type webdataset \
+    --train-data=/home/work/cc_ocr/data_3k/train-000000.tar  \
+    --val-data=/home/work/cc_ocr/data_3k/val-000000.tar  \
+    --batch-size=128 \
+    --lr=1e-4 \
+    --epochs=30 \
+    --workers=2 \
+    --train-num-samples 2400 \
+    --val-num-samples 600  \
+    --device cuda  \
+    --report-to wandb  \
+    --wandb-project-name KT_VisionAI_Intern_Project \
+    --log-every-n-steps 100 \
+    --logs=/home/work/VisionAI_Intern_Project/open_clip_vision_enc/scripts/logs
